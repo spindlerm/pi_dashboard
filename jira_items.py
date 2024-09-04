7d8a5f1f5ef41c76555a7b6570b9826db7a7bb8f
Matt Spindler <matthew.spindler@preservica.com>
2024-09-04 15:12:29 +0100

    Clean up comments

--- jira_items.py
+++ jira_items.py
@@ -51,9 +51,6 @@ def loadItems(config):
                 else:
                     fix_versions = fix_versions + "," + str(fixversion.name)
 
-            #sql = "INSERT INTO {0} (issueKey, issuetype, summary, fixversion, status) VALUES (%s,%s,%s,%s,%s)".format(config["jira"]["db_table"])
-
-            #val = (issue.key, str(issue.get_field("issuetype")), issue.get_field("summary"), fix_versions, str(issue.get_field("status")))
             sql = "INSERT INTO {0} (issueKey, issuetype, summary, fixversion, status) VALUES (%s,%s,%s,%s,%s) ON DUPLICATE KEY UPDATE  issuetype=%s,summary=%s,fixversion=%s,status=%s".format(config["jira"]["db_table"])
 
             print(sql)
