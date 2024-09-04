83c3b3721000a229834dab8c9d688273cbed6e83
Matt Spindler <matthew.spindler@preservica.com>
2024-09-04 15:08:12 +0100

    Update load.sql, to create tables with correct charset and collation

--- load.sql
+++ load.sql
@@ -2,7 +2,7 @@ use dashboard;
 drop table b1;
 drop table jira;
 
-CREATE TABLE IF NOT EXISTS b1(
+CREATE TABLE IF NOT EXISTS b1 (
 	id int,
 	number int,
 	startDate DATETIME,
@@ -11,15 +11,15 @@ CREATE TABLE IF NOT EXISTS b1(
 	branch varchar(100),
 	status varchar(20),
 	PRIMARY KEY(id)
-);
+) DEFAULT  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 
 
-CREATE TABLE IF NOT EXISTS jira(
+CREATE TABLE IF NOT EXISTS jira  (
 	issueKey varchar(50),
 	issuetype varchar(200),
 	summary varchar(200),
 	status varchar(20),
 	fixversion varchar(50),
 	PRIMARY KEY(issueKey)
-);
+) DEFAULT  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
